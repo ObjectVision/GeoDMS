aee933e0b3294ee5e82df20e0ea1c2360c42ea4e
eoudejans <eoudejans@objectvision.nl>
2022-10-24 09:49:40 +0200

    Changed background color of toolbar window.

--- exe/gui/src/GuiToolbar.cpp
+++ exe/gui/src/GuiToolbar.cpp
@@ -194,9 +194,12 @@ void GuiToolbar::ShowTableViewButtons(GuiView& view)
 void GuiToolbar::Update(bool* p_open, GuiView& view) // TODO: add int return to button which is its group. Untoggle all buttons in the same group.
 {
     ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0, 0));
+    ImGui::PushStyleColor(ImGuiCol_WindowBg, ImVec4(117.0/255.0, 117.0/255.0, 138.0/255.0, 1.0)); // , , 138/255, 0.0)
     if (!ImGui::Begin("Toolbar", p_open, ImGuiWindowFlags_HorizontalScrollbar))
     {
         ImGui::End();
+        ImGui::PopStyleVar();
+        ImGui::PopStyleColor();
         return;
     }
    
@@ -218,4 +221,5 @@ void GuiToolbar::Update(bool* p_open, GuiView& view) // TODO: add int return to 
 
     ImGui::End();
     ImGui::PopStyleVar();
+    ImGui::PopStyleColor();
 }
