a4ddbbcf54b8a5236075136342a2588e41f46e08
eoudejans <eoudejans@objectvision.nl>
2022-10-25 22:47:25 +0200

    Cleanup of file menu.

--- exe/gui/src/GuiMenu.cpp
+++ exe/gui/src/GuiMenu.cpp
@@ -217,29 +217,6 @@ void GuiMenuFileComponent::Update()
         //if (ImGui::MenuItem("Open Configuration File", "Ctrl+O")) 
         //    m_fileDialog.Open();
 
-        if (ImGui::Button("Delete.."))
-            ImGui::OpenPopup("Delete?");
-
-        if (ImGui::BeginPopupModal("Delete?", NULL, ImGuiWindowFlags_AlwaysAutoResize))
-        {
-            ImGui::Text("All those beautiful files will be deleted.\nThis operation cannot be undone!\n\n");
-            ImGui::Separator();
-
-            //static int unused_i = 0;
-            //ImGui::Combo("Combo", &unused_i, "Delete\0Delete harder\0");
-
-            static bool dont_ask_me_next_time = false;
-            ImGui::PushStyleVar(ImGuiStyleVar_FramePadding, ImVec2(0, 0));
-            ImGui::Checkbox("Don't ask me next time", &dont_ask_me_next_time);
-            ImGui::PopStyleVar();
-
-            if (ImGui::Button("OK", ImVec2(120, 0))) { ImGui::CloseCurrentPopup(); }
-            ImGui::SetItemDefaultFocus();
-            ImGui::SameLine();
-            if (ImGui::Button("Cancel", ImVec2(120, 0))) { ImGui::CloseCurrentPopup(); }
-            ImGui::EndPopup();
-        }
-
         if (ImGui::MenuItem("Open Configuration File", "Ctrl+O"))
         {
             auto file_name = StartWindowsFileDialog();
