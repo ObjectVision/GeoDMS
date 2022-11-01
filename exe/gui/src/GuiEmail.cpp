07ec23359e6363917420c6df8b86fab7d268cfd1
eoudejans <eoudejans@objectvision.nl>
2022-11-01 11:23:01 +0100

    Changed email command to use ShellExecute, in line with Delphi implementation.

--- exe/gui/src/GuiEmail.cpp
+++ exe/gui/src/GuiEmail.cpp
@@ -3,28 +3,24 @@
 #include "imgui_internal.h"
 #include "windows.h"
 
+#include <locale>
+#include <codecvt>
+
 bool GuiEmail::SendMailUsingDefaultWindowsEmailApplication(std::string message)
 {
-	// "support@objectvision.nl"
+	std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
 
+	std::wstring wstr_version = converter.from_bytes(DMS_GetVersion());
+	std::wstring wstr_message = converter.from_bytes(message);
+	std::wstring body = std::wstring(L"{You can insert an optional message here}%0D%0A") +
+									 L"==================================================%0D%0A" +
+									 L"DMS version: " + wstr_version + L"%0D%0A" +
+									 L"===================================================%0D%0A" +
+									 wstr_message;
 
+	std::wstring mailCommand =   std::wstring() + L"mailto:support@objectvision.nl"+ L"?subject=GeoDMS Bugreport&body=" + body;
 
-    // Format subject
-	std::string subject = "GeoDMS Bugreport";
-
-	// Format body
-	std::string body = std::string() + "{You can insert an optional message here}\n\n" +
-		"==================================================\n" +
-		"DMS version:" + DMS_GetVersion() + "\n" +
-		"===================================================\n" +
-		message;
-
-	// Format mailto: string
-	std::string mailCommand = "mailto:support@objectvision.nl"; //+ 
-												//"?subject=" + subject + 
-												//"&body=" + body;
-
-	auto result = ShellExecute(NULL, 0, (LPCWSTR)mailCommand.c_str(), (LPCWSTR)"", (LPCWSTR)"", SW_SHOWNORMAL); //SW_SHOWNOACTIVATE);
+	auto result = ShellExecute(0, NULL, mailCommand.c_str(), NULL, NULL, SW_SHOWNORMAL); //SW_SHOWNOACTIVATE);
 
 	return true;
 }
