204a61b64d292252631c25748ea76971064cbe0a
eoudejans <eoudejans@objectvision.nl>
2023-01-18 16:23:54 +0100

    Changed wstring to stdstring utf 8 implementation in GuiEmail

--- exe/gui/src/GuiEmail.cpp
+++ exe/gui/src/GuiEmail.cpp
@@ -7,7 +7,7 @@
 
 bool GuiEmail::SendMailUsingDefaultWindowsEmailApplication(std::string message)
 {
-	std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
+	/*std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
 
 	std::wstring wstr_version = converter.from_bytes(DMS_GetVersion());
 	std::wstring wstr_message = converter.from_bytes(message);
@@ -19,7 +19,17 @@ bool GuiEmail::SendMailUsingDefaultWindowsEmailApplication(std::string message)
 
 	std::wstring mailCommand =   std::wstring() + L"mailto:support@objectvision.nl"+ L"?subject=GeoDMS Bugreport&body=" + body;
 
-	auto result = ShellExecuteW(0, NULL, mailCommand.c_str(), NULL, NULL, SW_SHOWNORMAL); //SW_SHOWNOACTIVATE);
+	auto result = ShellExecuteW(0, NULL, mailCommand.c_str(), NULL, NULL, SW_SHOWNORMAL);
+	*/
+
+	std::string body1 = std::string("{You can insert an optional message here}%0D%0A") +
+		"==================================================%0D%0A" +
+		"DMS version: " + DMS_GetVersion() + "%0D%0A" +
+		"===================================================%0D%0A" +
+		message;
+
+	std::string mailCommand1 = std::string() + "mailto:support@objectvision.nl" + "?subject=GeoDMS Bugreport&body=" + body1;
+	auto result = ShellExecuteA(0, NULL, mailCommand1.c_str(), NULL, NULL, SW_SHOWNORMAL);
 
 	return true;
 }
