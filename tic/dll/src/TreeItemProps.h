fcfd8535e95263c7461eedc6756b163d727f8b6f
eoudejans <eoudejans@objectvision.nl>
2023-01-18 12:22:47 +0100

    Made TreeItem_GetSourceDescr accesible to external DLLs

--- tic/dll/src/TreeItemProps.h
+++ tic/dll/src/TreeItemProps.h
@@ -73,6 +73,6 @@ enum class SourceDescrMode {
 	All = 3
 };
 
-SharedStr TreeItem_GetSourceDescr(const TreeItem* studyObject, SourceDescrMode sdm, bool bShowHidden); // defined in SourceDescr.cpp
+TIC_CALL SharedStr TreeItem_GetSourceDescr(const TreeItem* studyObject, SourceDescrMode sdm, bool bShowHidden); // defined in SourceDescr.cpp
 
 #endif // __TIC_TREEITEMPROPS_H
