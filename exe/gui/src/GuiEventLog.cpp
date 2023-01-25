5281451c4ae15e84205694b0dff25e2fbbd1578e
eoudejans <eoudejans@objectvision.nl>
2023-01-25 16:23:55 +0100

    Supplemental cleanup of unused test code.

--- exe/gui/src/GuiEventLog.cpp
+++ exe/gui/src/GuiEventLog.cpp
@@ -93,16 +93,11 @@ auto GuiEventLog::DrawItem(EventLogItem *item) -> void
 {
     ImVec4 color = ConvertSeverityTypeIDToColor(item->m_Severity_type);
     ImGui::PushStyleColor(ImGuiCol_Text, color);
-    //ImGui::InputText("", &item->m_Text, 256, ImGuiInputTextFlags_ReadOnly, NULL, NULL);
-    //ImGui::InputText("", item->m_Text.c_str(), item->m_Text.size(), 0, NULL, NULL);
-    //ImGui::InputText("", &item->m_Text, ImGuiInputTextFlags_ReadOnly, nullptr, nullptr);
-    //ImGui::InputText("", item->m_Text.c_str());
     ImGui::TextUnformatted(item->m_Text.c_str());
     if (ImGui::IsItemClicked())
     { 
         ImGui::SetClipboardText(item->m_Text.c_str());
     }
-    //ImGui::Selectable()
     ImGui::PopStyleColor();
 }
 
