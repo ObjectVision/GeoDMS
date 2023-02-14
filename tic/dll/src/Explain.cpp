c35d4c95ee5eeca1cbfbb40ec3aec27f2fb754ad
Maarten Hilferink <mhilferink@objectvision.nl>
2023-02-14 18:35:15 +0100

    Explain, selection now works

--- tic/dll/src/Explain.cpp
+++ tic/dll/src/Explain.cpp
@@ -83,23 +83,23 @@ namespace Explain { // local defs
 	struct AbstrCalcExplanation
 	{
 		SharedDataItemInterestPtr             m_DataItem;
-		const AbstrUnit*                      m_UltimateDomainUnit;
-		const AbstrUnit*                      m_UltimateValuesUnit;
+		const AbstrUnit* m_UltimateDomainUnit;
+		const AbstrUnit* m_UltimateValuesUnit;
 		CoordinateCollectionType              m_Coordinates;
 		mutable SharedDataItemInterestPtrTuple m_Interests;
 		mutable GuiReadLockPair               m_UnitLabelLocks;
 		AbstrCalcExplanation(const AbstrDataItem* dataItem)
-			:	m_DataItem(dataItem)
-			,	m_UltimateDomainUnit(AsUnit(dataItem->GetAbstrDomainUnit()->GetUltimateItem()))
-			,	m_UltimateValuesUnit(AsUnit(dataItem->GetAbstrValuesUnit()->GetUltimateItem()))
+			: m_DataItem(dataItem)
+			, m_UltimateDomainUnit(AsUnit(dataItem->GetAbstrDomainUnit()->GetUltimateItem()))
+			, m_UltimateValuesUnit(AsUnit(dataItem->GetAbstrValuesUnit()->GetUltimateItem()))
 
 		{}
-		virtual ~AbstrCalcExplanation() 
+		virtual ~AbstrCalcExplanation()
 		{
 			reportD(SeverityTypeID::ST_MinorTrace, "Byte");
 		}
 
-		virtual ArgRef GetCalcDataItem(Context* context) const =0;
+		virtual ArgRef GetCalcDataItem(Context* context) const = 0;
 
 		CoordinateType* AddIndex(SizeT index) // returns nullptr if abundant
 		{
@@ -110,135 +110,37 @@ namespace Explain { // local defs
 			if (m_Coordinates.size() >= MaxNrEntries)
 				return nullptr;
 
-			m_Coordinates.push_back(CoordinateType(index, AbstrValueRef() ) ); // calculate later
+			m_Coordinates.push_back(CoordinateType(index, AbstrValueRef())); // calculate later
 			return &m_Coordinates.back();
 		}
 
-		const AbstrValue* CalcValue(Context* context) // returns nullptr if suspended
-		{
-			dms_assert(context);
-			dms_assert(context->m_CalcExpl);
-			dms_assert(context->m_Domain == m_UltimateDomainUnit);
-			CoordinateType* crd = context->m_Coordinate;
-			dms_assert(crd);
-			dms_assert(!crd->second);
+		const AbstrValue* CalcValue(Context* context); // returns nullptr if suspended
 
-			SizeT entityNr = crd->first;
-			if (!IsDefined(entityNr))
-				context = nullptr;
+		void GetDescr(CalcExplImpl* self, OutStreamBase& stream, bool isFirst, bool showHidden) const;
 
-			try {
-				auto result = GetCalcDataItem(context);
-				auto resultData = GetItem(result);
-				if (!resultData)
-				{
-					dms_assert(SuspendTrigger::DidSuspend());
-					return nullptr; // calc suspended
-				}
-				dms_assert(resultData);
-				resultData->UpdateMetaInfo();
-
-				if (!resultData->PrepareDataUsage(DrlType::Suspendible))
-				{
-					if (resultData->WasFailed(FR_Data))
-						resultData->ThrowFail();
-					dms_assert(SuspendTrigger::DidSuspend());
-					return nullptr;
-				}
-
-				if (!IsDefined(entityNr))
-				{
-					static SharedStr nullStr("<null>");
-					crd->second.assign( new ValueWrap<SharedStr>(nullStr) );
-				}
-				else
-				{
-					if (!WaitForReadyOrSuspendTrigger(resultData->GetCurrUltimateItem()))
-						return nullptr;
-
-					DataReadLock dlr(AsDataItem(resultData));
-					dms_assert(dlr.IsLocked());
-
-					const AbstrDataObject* resultObj = dlr.get_ptr();
-					if (not (entityNr < resultObj->GetTiledRangeData()->GetRangeSize()))
-					{
-						static SharedStr oorStr("<OutOfRange>");
-						crd->second = new ValueWrap<SharedStr>(oorStr);
-					}
-					else
-					{
-						OwningPtr<AbstrValue> valuePtr( resultObj->CreateAbstrValue() );
-
-						resultObj->GetAbstrValue(crd->first, *valuePtr);
-
-						crd->second = std::move( valuePtr );
-					}
-				}
-			}
-			catch (const DmsException& x)
-			{
-				crd->second = OwningPtr<AbstrValue>( new ValueWrap<SharedStr>(x.GetAsText()) );
-			}
-
-			dms_assert(crd->second);
-			return crd->second;
-		}
-
-		virtual void GetDescr(CalcExplImpl* self, OutStreamBase& stream, bool isFirst, bool showHidden) const = 0;
+		virtual void GetDescrImpl(CalcExplImpl* self, OutStreamBase& stream, bool isFirst, bool showHidden) const = 0;
 		virtual void PrintSeqNr(OutStreamBase& stream) const = 0;
 
-	protected:
-		void GetDescrBase(OutStreamBase& stream, bool isFirst, const AbstrUnit* domainUnit, const AbstrUnit* valuesUnit) const
+		auto RelativeExprPath() const -> SharedStr
 		{
-			if (domainUnit->IsKindOf(Unit<Void>::GetStaticClass()))
-				domainUnit = nullptr;
-
-			SizeT n = m_Coordinates.size();
-			stream << "Selected value"; if (n!=1) stream << "s";
-			NewLine(stream);
-
-			XML_Table tab(stream);
-			{
-				XML_Table::Row row(tab);
-				row.ClickableCell(
-					domainUnit ? domainUnit->GetName().c_str() : "",
-					domainUnit ? ItemUrl(domainUnit).c_str() : "");
-				row.ClickableCell(valuesUnit->GetName().c_str(), ItemUrl(valuesUnit).c_str());
-			}
-
-			for (SizeT i=0; i!=n; ++i)
-			{
-				SizeT recno = m_Coordinates[i].first;
-
-				SharedStr locStr = (domainUnit)
-					?	DisplayValue(
-							domainUnit, 
-							recno,
-							true,
-							m_Interests.m_DomainLabel, MAX_TEXTOUT_SIZE, m_UnitLabelLocks.first
-						)
-					:	SharedStr();
-
-				const AbstrValue* valuesValue = m_Coordinates[i].second;
-
-				SharedStr valStr;
-				if (valuesValue)
-					valStr = DisplayValue(valuesUnit, valuesValue, true, m_Interests.m_valuesLabel, MAX_TEXTOUT_SIZE, m_UnitLabelLocks.second);
-				else
-					valStr = calculatingStr;
-
-				if (n == 1 && isFirst || m_DataItem->IsCacheItem())
-					tab.NameValueRow(locStr.c_str(), valStr.c_str());
-				else
-				{
-					XML_Table::Row row(tab);
-					stream.WriteAttr("bgcolor", CLR_HROW);
-					SharedStr explainUrl = mySSPrintF("dms:dp.VI.ATTR!%d:%s", recno, m_DataItem->GetFullName().c_str());
-					row.ClickableCell(locStr.c_str(), explainUrl.c_str());
-					row.ClickableCell(valStr.c_str(), explainUrl.c_str());
-				}
-			}
+			VectorOutStreamBuff buff;
+			OutStream_DMS x(&buff, nullptr);
+			PrintSeqNr(x);
+			return SharedStr(buff.GetData(), buff.GetDataEnd());
 		}
+		bool MatchesExtraInfo(std::string_view extraInfo) const
+		{
+			if (extraInfo.empty())
+				return true;
+			auto relPath = RelativeExprPath();
+			auto extraInfoLen = extraInfo.size();
+			if (relPath.ssize() < extraInfoLen)
+				return false;
+			return strncmp(relPath.c_str(), extraInfo.data(), extraInfoLen) == 0;
+		}
+
+	protected:
+		void GetDescrBase(CalcExplImpl* self, OutStreamBase& stream, bool isFirst, const AbstrUnit* domainUnit, const AbstrUnit* valuesUnit) const;
 	};
 
 	struct DataCalcExplanation : AbstrCalcExplanation
@@ -249,7 +151,7 @@ namespace Explain { // local defs
 
 		ArgRef GetCalcDataItem(Context* context) const override { return ArgRef(std::in_place_type<SharedTreeItem>, m_DataItem.get_ptr()); }
 
-		void GetDescr(CalcExplImpl* self, OutStreamBase& stream, bool isFirst, bool showHidden) const override
+		void GetDescrImpl(CalcExplImpl* self, OutStreamBase& stream, bool isFirst, bool showHidden) const override
 		{
 			NewLine(stream);
 			{
@@ -259,7 +161,7 @@ namespace Explain { // local defs
 			NewLine(stream);
 
 
-			GetDescrBase(stream, isFirst, m_DataItem->GetAbstrDomainUnit(), m_DataItem->GetAbstrValuesUnit());
+			GetDescrBase(self, stream, isFirst, m_DataItem->GetAbstrDomainUnit(), m_DataItem->GetAbstrValuesUnit());
 
 			if (!isFirst)
 				return;
@@ -302,11 +204,11 @@ namespace Explain { // local defs
 			return CalcResult(m_CalcPtr, AbstrDataItem::GetStaticClass());
 		}
 
-		void GetDescr(CalcExplImpl* self, OutStreamBase& stream, bool isFirst, bool showHidden) const override;
+		void GetDescrImpl(CalcExplImpl* self, OutStreamBase& stream, bool isFirst, bool showHidden) const override;
 
 		virtual void AddLispExplanations(CalcExplImpl* self, LispPtr lispExprPtr, UInt32 level);
 
-		void PrintSeqNr(OutStreamBase& stream) const
+		void PrintSeqNr(OutStreamBase& stream) const override
 		{
 //			assert(m_Parent);
 			if (m_Parent) {
@@ -317,13 +219,6 @@ namespace Explain { // local defs
 					stream << ".";
 			}
 			stream << AsString(m_SeqNr).c_str();
-		}
-		auto RelativeExprPath() const -> SharedStr
-		{
-			VectorOutStreamBuff buff;
-			OutStream_DMS x(&buff, nullptr);
-			PrintSeqNr(x);
-			return SharedStr(buff.GetData(), buff.GetDataEnd());
 		}
 
 		DataControllerRef                m_DC;
@@ -411,7 +306,7 @@ namespace Explain { // local defs
 			return symbID == token::mul ;
 		}
 
-		void GetDescr(CalcExplImpl* self, OutStreamBase& stream, bool isFirst, bool showHidden) const override;
+		void GetDescrImpl(CalcExplImpl* self, OutStreamBase& stream, bool isFirst, bool showHidden) const override;
 		void AddLispExplanations(CalcExplImpl* self, LispPtr lispExprPtr, UInt32 level) override;
 	};
 
@@ -447,6 +342,8 @@ namespace Explain { // local defs
 			m_StudyObject = studyObject;
 			m_StudyIdx    = studyIdx;
 			m_ExtraInfo   = extraInfo;
+			m_ExprSeqNr   = 0;
+			m_ExprLevel   = 0;
 			m_Queue.clear();
 			m_DoneQueueEntries = m_DoneExpl = 0;
 
@@ -609,7 +506,7 @@ namespace Explain { // local defs
 				if (metaInfo.index() == 2)
 					AddExplanation(AsDataItem(std::get<SharedTreeItem>(metaInfo).get_ptr()));
 				if (metaInfo.index() == 1)
-					AddLispExplanation(std::get<LispRef>(metaInfo), 0, nullptr, 1);
+					AddLispExplanation(std::get<LispRef>(metaInfo), 0, nullptr, ++m_ExprSeqNr);
 			}
 		}
 
@@ -647,15 +544,11 @@ namespace Explain { // local defs
 //			auto errMsg = mySSPrintF("Unexpected key: %s", AsFLispSharedStr(key).c_str());
 //			throwCheckFailed(MG_POS, errMsg.c_str());
 		}
-		auto URL(const LispCalcExplanation* expl) -> SharedStr
+		auto URL(const LispCalcExplanation* expl, SizeT recNo) -> SharedStr
 		{
 			auto fullPath = SharedStr(m_StudyObject->GetFullName());
 			auto relExprPath = expl->RelativeExprPath();
-			return mySSPrintF("dms:dp.VI.ATTR!%d:%s?%s"
-				, m_StudyIdx
-				, fullPath.c_str()
-				, relExprPath.c_str()
-			);
+			return mySSPrintF("dms:dp.VI.ATTR!%d:%s?%s", recNo, fullPath.c_str(), relExprPath.c_str());
 		}
 
 		ExplArray                m_Expl;
@@ -669,6 +562,8 @@ namespace Explain { // local defs
 		SharedDataItem           m_StudyObject = 0;
 		SizeT                    m_StudyIdx = 0;
 		std::string              m_ExtraInfo;
+		arg_index                m_ExprSeqNr  = 0;
+		arg_index                m_ExprLevel  = 0;
 		TimeStamp                m_LastChange = 0;
 
 		friend struct CalcExplanations;
@@ -773,7 +668,7 @@ namespace Explain { // local defs
 	{
 		assert(self);		
 		for (arg_index seqNr = 0; lispExprPtr.IsRealList(); lispExprPtr = lispExprPtr->Right())
-			self->AddLispExplanation(lispExprPtr->Left(), level, m_Parent, ++seqNr);
+			self->AddLispExplanation(lispExprPtr->Left(), level, this, ++seqNr);
 
 		dms_assert(lispExprPtr.EndP());
 	}
@@ -783,28 +678,163 @@ namespace Explain { // local defs
 		arg_index seqNr = 0;
 		for (auto& term: m_Expr)
 			for (auto& factor: term.second)
-				self->AddLispExplanation(factor, level, m_Parent, ++seqNr);
+				self->AddLispExplanation(factor, level, this, ++seqNr);
 	}
 
-	void LispCalcExplanation::GetDescr(CalcExplImpl* self, OutStreamBase& stream, bool isFirst, bool showHidden) const
+	const AbstrValue* AbstrCalcExplanation::CalcValue(Context* context) // returns nullptr if suspended
+	{
+		dms_assert(context);
+		dms_assert(context->m_CalcExpl);
+		dms_assert(context->m_Domain == m_UltimateDomainUnit);
+		CoordinateType* crd = context->m_Coordinate;
+		dms_assert(crd);
+		dms_assert(!crd->second);
+
+		SizeT entityNr = crd->first;
+		if (!IsDefined(entityNr))
+			context = nullptr;
+
+		try {
+			auto result = GetCalcDataItem(context);
+			auto resultData = GetItem(result);
+			if (!resultData)
+			{
+				dms_assert(SuspendTrigger::DidSuspend());
+				return nullptr; // calc suspended
+			}
+			dms_assert(resultData);
+			resultData->UpdateMetaInfo();
+
+			if (!resultData->PrepareDataUsage(DrlType::Suspendible))
+			{
+				if (resultData->WasFailed(FR_Data))
+					resultData->ThrowFail();
+				dms_assert(SuspendTrigger::DidSuspend());
+				return nullptr;
+			}
+
+			if (!IsDefined(entityNr))
+			{
+				static SharedStr nullStr("<null>");
+				crd->second.assign(new ValueWrap<SharedStr>(nullStr));
+			}
+			else
+			{
+				if (!WaitForReadyOrSuspendTrigger(resultData->GetCurrUltimateItem()))
+					return nullptr;
+
+				DataReadLock dlr(AsDataItem(resultData));
+				dms_assert(dlr.IsLocked());
+
+				const AbstrDataObject* resultObj = dlr.get_ptr();
+				if (not (entityNr < resultObj->GetTiledRangeData()->GetRangeSize()))
+				{
+					static SharedStr oorStr("<OutOfRange>");
+					crd->second = new ValueWrap<SharedStr>(oorStr);
+				}
+				else
+				{
+					OwningPtr<AbstrValue> valuePtr(resultObj->CreateAbstrValue());
+
+					resultObj->GetAbstrValue(crd->first, *valuePtr);
+
+					crd->second = std::move(valuePtr);
+				}
+			}
+		}
+		catch (const DmsException& x)
+		{
+			crd->second = OwningPtr<AbstrValue>(new ValueWrap<SharedStr>(x.GetAsText()));
+		}
+
+		dms_assert(crd->second);
+		return crd->second;
+	}
+
+	void AbstrCalcExplanation::GetDescrBase(CalcExplImpl* self, OutStreamBase& stream, bool isFirst, const AbstrUnit* domainUnit, const AbstrUnit* valuesUnit) const
+	{
+		if (domainUnit->IsKindOf(Unit<Void>::GetStaticClass()))
+			domainUnit = nullptr;
+
+		SizeT n = m_Coordinates.size();
+		stream << "Selected value"; if (n != 1) stream << "s";
+		NewLine(stream);
+
+		XML_Table tab(stream);
+		{
+			XML_Table::Row row(tab);
+			row.ClickableCell(
+				domainUnit ? domainUnit->GetName().c_str() : "",
+				domainUnit ? ItemUrl(domainUnit).c_str() : "");
+			row.ClickableCell(valuesUnit->GetName().c_str(), ItemUrl(valuesUnit).c_str());
+		}
+
+		for (SizeT i = 0; i != n; ++i)
+		{
+			SizeT recno = m_Coordinates[i].first;
+
+			SharedStr locStr = (domainUnit)
+				? DisplayValue(
+					domainUnit,
+					recno,
+					true,
+					m_Interests.m_DomainLabel, MAX_TEXTOUT_SIZE, m_UnitLabelLocks.first
+				)
+				: SharedStr();
+
+			const AbstrValue* valuesValue = m_Coordinates[i].second;
+
+			SharedStr valStr;
+			if (valuesValue)
+				valStr = DisplayValue(valuesUnit, valuesValue, true, m_Interests.m_valuesLabel, MAX_TEXTOUT_SIZE, m_UnitLabelLocks.second);
+			else
+				valStr = calculatingStr;
+
+			if (n == 1 && isFirst)
+			{
+				tab.NameValueRow(locStr.c_str(), valStr.c_str());
+				return;
+			}
+
+			XML_Table::Row row(tab);
+			stream.WriteAttr("bgcolor", CLR_HROW);
+			SharedStr explainUrl;
+			if (m_DataItem->IsCacheItem())
+			{
+				auto lispExpr = dynamic_cast<const LispCalcExplanation*>(this);
+				if (lispExpr)
+					explainUrl = self->URL(lispExpr, recno);
+			}
+			else
+				explainUrl = mySSPrintF("dms:dp.VI.ATTR!%d:%s", recno, m_DataItem->GetFullName().c_str());
+			row.ClickableCell(locStr.c_str(), explainUrl.c_str());
+			row.ClickableCell(valStr.c_str(), explainUrl.c_str());
+		}
+	}
+
+	void AbstrCalcExplanation::GetDescr(CalcExplImpl* self, OutStreamBase& stream, bool isFirst, bool showHidden) const
+	{
+		if (!MatchesExtraInfo(self->m_ExtraInfo))
+			return;
+		if (self->m_ExprLevel > 3)
+			return;
+		DynamicIncrementalLock lock(self->m_ExprLevel);
+		GetDescrImpl(self, stream, isFirst, showHidden);
+	}
+
+	void LispCalcExplanation::GetDescrImpl(CalcExplImpl* self, OutStreamBase& stream, bool isFirst, bool showHidden) const
 	{
 		dms_assert(!isFirst);
 		NewLine(stream);
 		stream << "Expr ";
 		PrintSeqNr(stream);
-		stream << " (in FLisp format): ";
-		{
-			auto valueURL = self->URL(this);
-
-			XML_hRef xmlElemA(stream, valueURL.c_str());
-			stream.WriteTrimmed(m_CalcPtr->GetAsFLispExprOrg().c_str());
-		}
+		stream << " (in FLisp format): "; stream.WriteTrimmed(m_CalcPtr->GetAsFLispExprOrg().c_str());
 		NewLine(stream);
 
-		GetDescrBase(stream, isFirst, m_UltimateDomainUnit, m_UltimateValuesUnit);
+		GetDescrBase(self, stream, isFirst, m_UltimateDomainUnit, m_UltimateValuesUnit);
 	}
 
-	void SumOfTermsExplanation::GetDescr(CalcExplImpl* self, OutStreamBase& stream, bool isFirst, bool showHidden) const
+	void SumOfTermsExplanation::GetDescrImpl(CalcExplImpl* self, OutStreamBase& stream, bool isFirst, bool showHidden) const
 	{
 		dms_assert(!isFirst);
 		NewLine(stream);
@@ -853,7 +883,7 @@ namespace Explain { // local defs
 				auto expl = self->FindExpl(factor);
 				if (expl)
 				{
-					auto valueURL = self->URL(expl);
+					auto valueURL = self->URL(expl, expl->m_Coordinates[0].first);
 					auto valuePtr = expl->m_Coordinates[0].second.get();
 					if (valuePtr)
 						row.ClickableCell(valuePtr->AsString().c_str(), valueURL.c_str());
