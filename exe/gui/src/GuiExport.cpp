6809ae2da5d72d5701dea897d689505662abb726
eoudejans <eoudejans@objectvision.nl>
2023-03-02 17:03:20 +0100

    Implemented export for raster/vector empty functionality.

--- exe/gui/src/GuiExport.cpp
+++ exe/gui/src/GuiExport.cpp
@@ -4,6 +4,7 @@
 #include "AbstrDataItem.h"
 #include "AbstrDataObject.h"
 #include "AbstrUnit.h"
+#include "utl/Environment.h"
 
 bool CurrentItemCanBeExportedToVector(TreeItem* item)
 {
@@ -29,37 +30,144 @@ bool CurrentItemCanBeExportedToRaster(TreeItem* item)
     return false;
 }
 
+void GuiExport::SetStorageLocation()
+{
+    ImGui::Text("Output folder:   "); ImGui::SameLine();
+
+    if (ImGui::InputText("##LocalDataDir", &m_folder_name, ImGuiInputTextFlags_None, InputTextCallback, nullptr))
+    {
+    }
+    ImGui::SameLine();
+    ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0.f, 0.f, 0.f, 0.f));
+    ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4(0.f, 0.f, 0.f, 0.f));
+    ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4(0.f, 0.f, 0.f, 0.f));
+
+    if (ImGui::Button(ICON_RI_FOLDER_OPEN))
+    {
+        auto tmp_folder_name = BrowseFolder(GetLocalDataDir().c_str());//StartWindowsFileDialog(ConvertDmsFileNameAlways(GetGeoDmsRegKey("LocalDataDir")).c_str(), L"test", L"*.*");
+        if (!tmp_folder_name.empty())
+        {
+            m_folder_name = tmp_folder_name;
+        }
+    }
+    ImGui::PopStyleColor(3);
+
+    ImGui::TextUnformatted("Filename: ");
+    ImGui::SameLine();
+    if (ImGui::InputText("##export_filename", &m_file_name, ImGuiInputTextFlags_EnterReturnsTrue, InputTextCallback, nullptr))
+    {
+    }
+}
+
+GuiExport::GuiExport()
+{
+    m_folder_name = GetLocalDataDir().c_str();
+}
+
 void GuiExport::Update(bool* p_open, GuiState &state)
 {
     ImGui::SetNextWindowSize(ImVec2(800, 400), ImGuiCond_Once);
-    if (ImGui::Begin("Export", p_open, ImGuiWindowFlags_None | ImGuiWindowFlags_NoDocking | ImGuiWindowFlags_NoTitleBar))
+    if (!ImGui::Begin("Export", p_open, ImGuiWindowFlags_None | ImGuiWindowFlags_NoDocking | ImGuiWindowFlags_NoTitleBar))
     {
-        if (ImGui::IsWindowHovered() && ImGui::IsMouseReleased(ImGuiMouseButton_Left))
-            SetKeyboardFocusToThisHwnd();
+        ImGui::End();
+        return;
+    }
 
-        bool enable_vector_export = CurrentItemCanBeExportedToVector(state.GetCurrentItem());
-        bool enable_raster_export = CurrentItemCanBeExportedToRaster(state.GetCurrentItem());;
 
-        if (ImGui::BeginTabBar("ExportTypes", ImGuiTabBarFlags_None))
+    if (ImGui::IsWindowHovered() && ImGui::IsMouseReleased(ImGuiMouseButton_Left))
+        SetKeyboardFocusToThisHwnd();
+
+    bool enable_vector_export = CurrentItemCanBeExportedToVector(state.GetCurrentItem());
+    bool enable_raster_export = CurrentItemCanBeExportedToRaster(state.GetCurrentItem());;
+
+    if (ImGui::BeginTabBar("ExportTypes", ImGuiTabBarFlags_None))
+    {
+        ImGui::BeginDisabled(!enable_vector_export);
+        if (ImGui::BeginTabItem("Vector"))
         {
-            ImGui::BeginDisabled(!enable_vector_export);
-            if (ImGui::BeginTabItem("Vector"))
-            {
-                ImGui::EndTabItem();
-            }
-            ImGui::EndDisabled();
 
-            ImGui::BeginDisabled(!enable_raster_export);
-            if (ImGui::BeginTabItem("Raster"))
-            {
-                ImGui::EndTabItem();
-            }
-            ImGui::EndDisabled();
-
-            ImGui::EndTabBar();
+            SetStorageLocation();
+            ImGui::EndTabItem();
         }
 
 
-        ImGui::End();
+        ImGui::EndDisabled();
+
+        ImGui::BeginDisabled(!enable_raster_export);
+        if (ImGui::BeginTabItem("Raster"))
+        {
+            SetStorageLocation();
+            ImGui::EndTabItem();
+        }
+
+
+        ImGui::EndDisabled();
+
+        ImGui::EndTabBar();
     }
+
+
+    auto options_window_pos = ImGui::GetWindowPos();
+    auto options_window_content_region = ImGui::GetWindowContentRegionMax();
+    auto current_cursor_pos_X = ImGui::GetCursorPosX();
+    ImGui::SetCursorPos(ImVec2(current_cursor_pos_X, options_window_content_region.y - 1.5 * ImGui::GetTextLineHeight()));
+
+    if (ImGui::Button("Export", ImVec2(50, 1.5 * ImGui::GetTextLineHeight())))
+    {
+    }
+    ImGui::SameLine();
+    if (ImGui::Button("Cancel", ImVec2(50, 1.5 * ImGui::GetTextLineHeight())))
+    {
+    }
+
+    ImGui::End();
+
+    //if (ImGui::Begin("Export", p_open, ImGuiWindowFlags_None | ImGuiWindowFlags_NoDocking | ImGuiWindowFlags_NoTitleBar))
+   // {
+
+
+    /*if (ImGui::IsWindowHovered() && ImGui::IsMouseReleased(ImGuiMouseButton_Left))
+        SetKeyboardFocusToThisHwnd();
+
+    bool enable_vector_export = CurrentItemCanBeExportedToVector(state.GetCurrentItem());
+    bool enable_raster_export = CurrentItemCanBeExportedToRaster(state.GetCurrentItem());;
+
+    if (ImGui::BeginTabBar("ExportTypes", ImGuiTabBarFlags_None))
+    {
+        ImGui::BeginDisabled(!enable_vector_export);
+        if (ImGui::BeginTabItem("Vector"))
+        {
+
+            SetStorageLocation();
+            ImGui::EndTabItem();
+        }
+        ImGui::EndDisabled();
+
+        ImGui::BeginDisabled(!enable_raster_export);
+        if (ImGui::BeginTabItem("Raster"))
+        {
+            SetStorageLocation();
+            ImGui::EndTabItem();
+        }
+        ImGui::EndDisabled();
+
+        ImGui::EndTabBar();
+    }
+
+    auto options_window_pos = ImGui::GetWindowPos();
+    auto options_window_content_region = ImGui::GetWindowContentRegionMax();
+    auto current_cursor_pos_X = ImGui::GetCursorPosX();
+    ImGui::SetCursorPos(ImVec2(current_cursor_pos_X, options_window_content_region.y - 1.5 * ImGui::GetTextLineHeight()));
+
+    if (ImGui::Button("Export", ImVec2(50, 1.5 * ImGui::GetTextLineHeight())))
+    {
+    }
+    ImGui::SameLine();
+    if (ImGui::Button("Cancel", ImVec2(50, 1.5 * ImGui::GetTextLineHeight())))
+    {
+    }
+
+
+    ImGui::End();
+    */
 }
--- exe/gui/src/GuiExport.h
+++ exe/gui/src/GuiExport.h
@@ -4,5 +4,12 @@
 class GuiExport
 {
 public:
+	GuiExport();
 	void Update(bool* p_open, GuiState& state);
+
+private:
+	void SetStorageLocation();
+
+	std::string m_folder_name;
+	std::string m_file_name;
 };
