22eba6ac4846b3580711cf6727bbf7a85e113ae1
eoudejans <eoudejans@objectvision.nl>
2023-05-23 14:13:27 +0200

    Added DMS_Shv_Load().

--- exe/gui/src/main_qt.cpp
+++ exe/gui/src/main_qt.cpp
@@ -2,13 +2,13 @@
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
 #include <QApplication>
 #include <QResource>
-
+#include "ShvDllInterface.h"
 
 #include "mainwindow.h"
 
 int main(int argc, char *argv[])
 {
-
+    DMS_Shv_Load();
     QApplication app(argc, argv);
     Q_INIT_RESOURCE(GeoDmsGuiQt);
     MainWindow main_window;
