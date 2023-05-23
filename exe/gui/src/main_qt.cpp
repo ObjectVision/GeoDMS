0a1899462ae56ee559d7a0c78f5d9643a3fd5ce3
eoudejans <eoudejans@objectvision.nl>
2023-05-23 14:22:03 +0200

    Added geodms window icon.

--- exe/gui/src/main_qt.cpp
+++ exe/gui/src/main_qt.cpp
@@ -6,12 +6,22 @@
 
 #include "mainwindow.h"
 
+int init_geodms()
+{
+
+    return 0;
+}
+
 int main(int argc, char *argv[])
 {
     DMS_Shv_Load();
+    DBG_INIT_COUT;
+
     QApplication app(argc, argv);
     Q_INIT_RESOURCE(GeoDmsGuiQt);
     MainWindow main_window;
+    main_window.setWindowIcon(QIcon(":res/images/GeoDmsGui-0.png"));
+
     //main_window.setWindowState(Qt::WindowMaximized);
     main_window.showMaximized();
     return app.exec();
