7ba0b9c4d712795d1254a7dcb7432150996bae84
eoudejans <eoudejans@objectvision.nl>
2023-06-28 13:09:45 +0200

    Treeview context menu called exec before all actions where registered.

--- qtgui/exe/src/DmsTreeView.cpp
+++ qtgui/exe/src/DmsTreeView.cpp
@@ -433,7 +433,6 @@ void DmsTreeView::showTreeviewContextMenu(const QPoint& pos)
 	auto map_view_action = MainWindow::TheOne()->m_mapview_action.get();
 	map_view_action->setEnabled(viewstyle_flags & ViewStyleFlags::vsfMapView);
 	m_context_menu->addAction(map_view_action);
-	m_context_menu->exec(viewport()->mapToGlobal(pos));
 
 	// statistics view
 	auto statistics_view_action = MainWindow::TheOne()->m_statistics_action.get();
@@ -450,7 +449,7 @@ void DmsTreeView::showTreeviewContextMenu(const QPoint& pos)
 	auto process_scheme = MainWindow::TheOne()->m_process_schemes_action.get();
 	process_scheme->setDisabled(true);
 	m_context_menu->addAction(process_scheme);
-
+	m_context_menu->exec(viewport()->mapToGlobal(pos));
 }
 
 void DmsTreeView::setNewCurrentItem(TreeItem* new_current_item)
