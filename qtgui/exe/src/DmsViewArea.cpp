8548a0f0adcba603cb8363a8287f42a3912d01a5
eoudejans <eoudejans@objectvision.nl>
2023-08-04 16:44:21 +0200

    Adding a layer / column to map/table view is now listed as command.

--- qtgui/exe/src/DmsViewArea.cpp
+++ qtgui/exe/src/DmsViewArea.cpp
@@ -165,8 +165,14 @@ QDmsViewArea::QDmsViewArea(QMdiArea* parent, TreeItem* viewContext, const TreeIt
     CreateDmsView(parent);
     // SHV_DataView_AddItem can call ClassifyJenksFisher, which requires DataView with a m_hWnd, so this must be after CreateWindowEx
     // or PostMessage(WM_PROCESS_QUEUE, ...) directly here to trigger DataView::ProcessGuiOpers()
-    try {
+    try 
+    {
+        auto current_item = MainWindow::TheOne()->getCurrentTreeItem();
         m_DataView->AddLayer(currItem, false);
+        if (m_DataView->GetViewType()== ViewStyle::tvsMapView)
+            reportF(MsgCategory::commands, SeverityTypeID::ST_MinorTrace, "Command: add layer for item %s in %s", current_item, m_DataView->GetCaption());
+        else
+            reportF(MsgCategory::commands, SeverityTypeID::ST_MinorTrace, "Command: add column for item %s in %s", current_item, m_DataView->GetCaption());
     }
     catch (...) {
         CloseWindow((HWND)m_DataViewHWnd); // calls SHV_DataView_Destroy
