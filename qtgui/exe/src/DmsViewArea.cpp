32c1ee7d2e8dff235a1e4c993e9506c5e87529ca
eoudejans <eoudejans@objectvision.nl>
2023-08-07 13:43:42 +0200

    Fixed issue #408, adding an item to a view which is incompatible to that view will now result in an error and no longer open the default view of that window.

--- qtgui/exe/src/DmsViewArea.cpp
+++ qtgui/exe/src/DmsViewArea.cpp
@@ -264,11 +264,11 @@ void QDmsViewArea::dropEvent(QDropEvent* event)
     if (tree_view != source)
         return;
 
-    auto current_ti = MainWindow::TheOne()->getCurrentTreeItem();
-    if (m_DataView->CanContain(current_ti))
+    auto current_item = MainWindow::TheOne()->getCurrentTreeItem();
+    if (m_DataView->CanContain(current_item))
         SHV_DataView_AddItem(m_DataView, MainWindow::TheOne()->getCurrentTreeItem(), false);
     else
-        MainWindow::TheOne()->defaultView();
+        reportF(MsgCategory::commands, SeverityTypeID::ST_Error, "Item %s is incompatible with view: %s", current_item->GetFullName(), m_DataView->GetCaption());
 }
 
 void QDmsViewArea::moveEvent(QMoveEvent* event)
