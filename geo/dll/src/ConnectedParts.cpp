690800805886a4945728dce5aaca448a90dfa2d2
Maarten Hilferink <mhilferink@objectvision.nl>
2024-03-31 01:03:24 +0100

    #574 code clean-up

--- geo/dll/src/ConnectedParts.cpp
+++ geo/dll/src/ConnectedParts.cpp
@@ -9,6 +9,7 @@
 #endif //defined(CC_PRAGMAHDRSTOP)
 
 #include "CheckedDomain.h"
+#include "TileChannel.h"
 #include "Unit.h"
 #include "UnitClass.h"
 
@@ -168,15 +169,12 @@ public:
 
 			ResultUnitType* resultUnit = debug_cast<ResultUnitType*>(res);
 			assert(resultUnit);
+			assert(nrParts <= nrV);
 			resultUnit->SetRange(Range<UInt32>(0, nrParts));
 
-			DataWriteLock resLock(resSub, dms_rw_mode::write_only_all);
-			auto resultSub = mutable_array_cast<PartType>(resLock);
-			auto realResultData = resultSub->GetDataWrite(no_tile, dms_rw_mode::write_only_all);
-			assert(realResultData.size() == nrV);
-			assert(nrParts <= nrV);
-			fast_copy(begin_ptr(resultData), end_ptr(resultData), realResultData.begin());
-			resLock.Commit();
+			auto resultSubChannel = locked_tile_write_channel<PartType>(resSub);
+			resultSubChannel.Write(begin_ptr(resultData), end_ptr(resultData));
+			resultSubChannel.Commit();
 		}
 		return true;
 	}
@@ -190,6 +188,144 @@ public:
 // Output: P { Part_rel: V->P; F { F1: F->P; F2: F->P } }
 
 #include "ptr/OwningPtrSizedArray.h"
+
+template<typename NodeType, typename LinkType, typename PartType, typename PartLinkType>
+auto stronglyConnectedComponentsIterativeWithInvertedLinks(NodeType nrV, LinkType nrE, const LinkType* link1, const LinkType* nextLink1, const NodeType* node2Data, PartType* resSubData)
+{
+	sequence_traits<Bool>::container_type onStackFlag(nrV, false);
+	std::vector<NodeType> nodeStack;
+	std::vector<Couple<PartType>> partLinks;
+
+	std::vector<std::tuple<NodeType, LinkType, bool>> stack; // Node, CurrentLink, didVisitLink
+	OwningPtrSizedArray< Couple < PartType >> timedParLinkLinkedList; // avoid using a std::set for the partLinks from currentPart
+	OwningPtrSizedArray<Couple<NodeType>> indices(nrV, dont_initialize MG_DEBUG_ALLOCATOR_SRC("stronglyConnectedComponents: indices"));
+	for (auto& index : indices)
+		index.first = UNDEFINED_VALUE(NodeType);
+	assert(indices.size() == nrV);
+
+	PartType nrParts = 0;
+	PartLinkType nrPartLinks = 0;
+	NodeType currIndex = 0;
+
+	auto pushNode = [&](NodeType v) {
+			// Initial visit actions (similar to the start of the recursive function)
+			indices[v] = { currIndex, currIndex }; // Set the depth index for v to the smallest unused index
+			++currIndex;
+			nodeStack.emplace_back(v);
+			assert(!onStackFlag[v]);
+			onStackFlag[v] = true;
+			stack.emplace_back(v, link1[v], false); // Start with first visit of v
+		};
+	for (NodeType v = 0; v != nrV; ++v)
+	{
+		if (IsDefined(indices[v].first))
+			continue;
+		pushNode(v);
+		while (!stack.empty()) {
+			auto& [v, currentLink, didVisitLink] = stack.back();
+			// Processing neighbors or finishing up
+		retry:
+			if (IsDefined(currentLink)) {
+				NodeType w = node2Data[currentLink];
+
+				if (didVisitLink)
+				{
+					MakeMin(indices[v].second, indices[w].second);
+					didVisitLink = false;
+				}
+				else
+				{
+					if (!IsDefined(indices[w].first)) {
+						// Neighbor w has not yet been visited, push to stack for processing
+						didVisitLink = true;
+						pushNode(w);
+						continue; // recurse by iteration
+					}
+					else if (onStackFlag[w].value()) {
+						// Successor w is in stack S and hence in the current SCC
+						// If w is not on stack, then (v, w) is an edge pointing to an SCC already found and must be ignored
+						// The next line may look odd - but is correct.
+						// It says w.index not w.lowlink; that is deliberate and from the original paper
+						MakeMin(indices[v].second, indices[w].first);
+					}
+					// Move to the next link
+				}
+				currentLink = nextLink1[currentLink];
+				goto retry; // continue processing neighbors, no do while loop to allow continue to jump to processing next or previous stack frame
+			}
+			// Final actions (similar to the end of the recursive function)
+			// If v is a root node, pop the stack and generate an SCC
+			if (indices[v].first == indices[v].second)
+			{
+				// start a new strongly connected component
+				// use nrParts (initially zero) as chrono-number for accessing timedParLinkLinkedList
+				if (nrParts >= timedParLinkLinkedList.size())
+				{
+					timedParLinkLinkedList = OwningPtrSizedArray< Couple < PartType> >(nrParts * 2, dont_initialize MG_DEBUG_ALLOCATOR_SRC("timedParLinkLinkedList"));
+					for (PartType i = 0; i != nrParts; ++i)
+						timedParLinkLinkedList[i].first = UNDEFINED_VALUE(NodeType);
+				}
+				else
+				{
+					timedParLinkLinkedList[nrParts - 1].first = UNDEFINED_VALUE(NodeType);
+				}
+				PartType lastSubPart = UNDEFINED_VALUE(PartType);
+
+				NodeType w;
+				do {
+					w = nodeStack.back(); nodeStack.pop_back();
+					assert(onStackFlag[w]);
+					onStackFlag[w] = false;
+					resSubData[w] = nrParts; // add w to current strongly connected component
+
+					for (LinkType e = link1[w]; IsDefined(e); e = nextLink1[e])
+					{
+						NodeType ww = node2Data[e];
+						PartType pp = resSubData[ww];
+						if (IsDefined(pp))
+						{
+							assert(pp <= nrParts);
+							if (pp < nrParts)
+							{
+								// set the chronos of pp to the current part to avoid adding pp to the partLinks of the current part more than once
+								// aka Lazy Initialization
+								if (timedParLinkLinkedList[pp].first != nrParts)
+								{
+									timedParLinkLinkedList[pp].second = lastSubPart;
+									lastSubPart = pp;
+									timedParLinkLinkedList[pp].first = nrParts;
+								}
+							}
+						}
+					}
+				} while (w != v);
+				while (IsDefined(lastSubPart))
+				{
+					partLinks.emplace_back(Couple<PartType>(nrParts, lastSubPart));
+					assert(timedParLinkLinkedList[lastSubPart].first == nrParts);
+					lastSubPart = timedParLinkLinkedList[lastSubPart].second;
+				}
+				++nrParts;
+			}
+			stack.pop_back();
+		}
+	}
+	return std::make_pair(nrParts, std::move(partLinks));
+}
+
+template<typename NodeType, typename LinkType, typename PartType, typename PartLinkType>
+auto stronglyConnectedComponentsIterative(NodeType nrV, LinkType nrE, const DataArray<NodeType>* arg1, const NodeType* node2Data, PartType* resSubData)
+{
+	std::vector<LinkType>
+		link1(nrV, UNDEFINED_VALUE(LinkType)),
+		nextLink1(nrE, UNDEFINED_VALUE(LinkType));
+	{
+		auto node1Data = arg1->GetDataRead();
+		assert(node1Data.size() == nrE);
+		InvertIntoLinkedList<NodeType, LinkType>(node1Data.begin(), link1.begin(), nextLink1.begin(), nrE, nrV);
+	}
+	return stronglyConnectedComponentsIterativeWithInvertedLinks<NodeType, LinkType, PartType, PartLinkType>(nrV, nrE, begin_ptr(link1), begin_ptr(nextLink1), node2Data, resSubData);
+}
 
 class StronglyConnectedComponentsOperator : public BinaryOperator
 {
@@ -205,8 +341,6 @@ class StronglyConnectedComponentsOperator : public BinaryOperator
 	using ResultSub1Type = DataArray<PartType>; // V->P
 	using ResultSub2Type = Unit<PartLinkType>; // F
 	using ResultSub2SubType = DataArray<PartType>; // F->P
-
-	using NodeStackType = std::vector<NodeType>;
 
 public:
 	StronglyConnectedComponentsOperator() :
@@ -246,185 +380,48 @@ public:
 		DataReadLock arg1Lock(arg1A);
 		DataReadLock arg2Lock(arg2A);
 
-		const Arg1Type* arg1 = const_array_cast<NodeType>(arg1Lock);
-		const Arg2Type* arg2 = const_array_cast<NodeType>(arg2Lock);
+		const Arg1Type* arg1 = const_array_cast<NodeType>(arg1Lock); assert(arg1);
+		const Arg2Type* arg2 = const_array_cast<NodeType>(arg2Lock); assert(arg2);
 
-		assert(arg1 && arg2);
+		auto linkDomainData = arg1->GetTiledRangeData(); assert(linkDomainData);
+		auto nodeDomainData = arg1->GetValueRangeData(); assert(nodeDomainData);
 
-		auto e = arg1->GetTiledRangeData();
-		auto v = arg1->GetValueRangeData();
-		assert(e && v);
-
-		auto node1Data = arg1->GetDataRead();
 		auto node2Data = arg2->GetDataRead();
 
-		NodeType nrV = v->GetElemCount();
-		LinkType nrE = e->GetElemCount();
+		NodeType nrV = nodeDomainData->GetElemCount();
+		LinkType nrE = linkDomainData->GetElemCount();
 
-		assert(node1Data.size() == nrE);
 		assert(node2Data.size() == nrE);
 
-		std::vector<PartType> resultData(nrV, UNDEFINED_VALUE(PartType));
-		std::vector<Couple<NodeType>> indices(nrV, Couple<NodeType>(Undefined()));
-		assert(resultData.size() == nrV);
-		assert(indices.size() == nrV);
-
-		std::vector<LinkType>
-			link1(nrV, UNDEFINED_VALUE(LinkType)),
-			nextLink1(nrE, UNDEFINED_VALUE(LinkType));
-
-		sequence_traits<Bool>::container_type onStackFlag(nrV, false);
-
-		InvertIntoLinkedList<NodeType, LinkType>(node1Data.begin(), link1.begin(), nextLink1.begin(), nrE, nrV);
-
-		NodeStackType nodeStack;
-		std::vector<Couple<PartType>> partLinks;
-
-		PartType nrParts = 0;
-		PartLinkType nrPartLinks = 0;
-		NodeType currIndex = 0;
-
-		auto resSubLock = CreateHeapTileArrayV<PartType>(v, nullptr, false MG_DEBUG_ALLOCATOR_SRC("StronglyConnectedComponentsOperator: resLock"));
-		auto resSubData = resSubLock->GetDataWrite(no_tile, dms_rw_mode::write_only_all);
+		OwningPtrSizedArray<PartType> resSubData(nrV, dont_initialize MG_DEBUG_ALLOCATOR_SRC("StronglyConnectedComponentsOperator: resSubData"));
 		fast_undefine(resSubData.begin(), resSubData.end());
 
-		std::vector<std::tuple<NodeType, LinkType, bool>> stack; // Node, CurrentLink, didVisitLink
-		OwningPtrSizedArray< std::pair < PartType, PartType>> timedParLinkLinkedList; // avoid using a std::set for the partLinks from currentPart
-
-		for (NodeType v = 0; v != nrV; ++v)
-		{
-			if (IsDefined(indices[v].first))
-				continue;
-			// Initial visit actions (similar to the start of the recursive function)
-			indices[v] = { currIndex, currIndex }; // Set the depth index for v to the smallest unused index
-			++currIndex;
-			nodeStack.emplace_back(v);
-			assert(!onStackFlag[v]);
-			onStackFlag[v] = true;
-			stack.emplace_back(v, link1[v], false); // Start with first visit of v
-			while (!stack.empty()) {
-				auto& [v, currentLink, didVisitLink] = stack.back();
-				// Processing neighbors or finishing up
-			retry:
-				if (IsDefined(currentLink)) {
-					NodeType w = node2Data[currentLink];
-
-					if (didVisitLink)
-					{ 
-						MakeMin(indices[v].second, indices[w].second);
-						didVisitLink = false;
-					}
-					else
-					{
-						if (!IsDefined(indices[w].first)) {
-							// Neighbor w has not yet been visited, push to stack for processing
-							didVisitLink = true;
-
-							// Initial visit actions (similar to the start of the recursive function)
-							indices[w] = { currIndex, currIndex }; // Set the depth index for v to the smallest unused index
-							++currIndex;
-							nodeStack.emplace_back(w);
-							assert(!onStackFlag[w]);
-							onStackFlag[w] = true;
-
-							stack.emplace_back(w, link1[w], false);
-							continue; // recurse by iteration
-						}
-						else if (onStackFlag[w].value()) {
-							// Successor w is in stack S and hence in the current SCC
-							// If w is not on stack, then (v, w) is an edge pointing to an SCC already found and must be ignored
-							// The next line may look odd - but is correct.
-							// It says w.index not w.lowlink; that is deliberate and from the original paper
-							MakeMin(indices[v].second, indices[w].first);
-						}
-						// Move to the next link
-					}
-					currentLink = nextLink1[currentLink];
-					goto retry; // continue processing neighbors, no do while loop to allow continue to jump to processing next or previous stack frame
-				}
-				// Final actions (similar to the end of the recursive function)
-				// If v is a root node, pop the stack and generate an SCC
-				if (indices[v].first == indices[v].second)
-				{
-					// start a new strongly connected component
-					// use nrParts (initially zero) as chrono-number for accessing timedParLinkLinkedList
-					if (nrParts >= timedParLinkLinkedList.size())
-					{
-						timedParLinkLinkedList = OwningPtrSizedArray< std::pair < PartType, PartType>>(nrParts * 2, dont_initialize MG_DEBUG_ALLOCATOR_SRC("timedParLinkLinkedList"));
-						for (PartType i = 0; i != nrParts; ++i)
-							timedParLinkLinkedList[i].first = UNDEFINED_VALUE(NodeType);
-					}
-					else
-					{
-						timedParLinkLinkedList[nrParts-1].first = UNDEFINED_VALUE(NodeType);
-					}
-					PartType lastSubPart = UNDEFINED_VALUE(PartType);
-
-					NodeType w;
-					do {
-						w = nodeStack.back(); nodeStack.pop_back();
-						assert(onStackFlag[w]);
-						onStackFlag[w] = false;
-						resSubData[w] = nrParts; // add w to current strongly connected component
-
-						for (LinkType e = link1[w]; IsDefined(e); e = nextLink1[e])
-						{
-							NodeType ww = node2Data[e];
-							PartType pp = resSubData[ww];
-							if (IsDefined(pp))
-							{
-								assert(pp <= nrParts);
-								if (pp < nrParts)
-								{
-									// set the chronos of pp to the current part to avoid adding pp to the partLinks of the current part more than once
-									// aka Lazy Initialization
-									if (timedParLinkLinkedList[pp].first != nrParts)
-									{
-										timedParLinkLinkedList[pp].second = lastSubPart;
-										lastSubPart = pp;
-										timedParLinkLinkedList[pp].first= nrParts; 
-									}
-								}
-							}
-						}
-					} while (w != v);
-					while (IsDefined(lastSubPart))
-					{
-						partLinks.emplace_back(Couple<PartType>(nrParts, lastSubPart));
-						assert(timedParLinkLinkedList[lastSubPart].first == nrParts);
-						lastSubPart = timedParLinkLinkedList[lastSubPart].second;
-					}
-					++nrParts;
-				}
-				stack.pop_back(); 
-			}
-		}
+		auto [nrParts, partLinks] = stronglyConnectedComponentsIterative<NodeType, LinkType, PartType, PartLinkType>(nrV, nrE
+			, arg1
+			, node2Data.begin()
+			, resSubData.begin());
 
 		ResultUnitType* resultUnit = debug_cast<ResultUnitType*>(res);
 		assert(resultUnit);
 		resultUnit->SetRange(Range<PartType>(0, nrParts));
 
-		resSubLock->InitValueRangeData(resultUnit->m_RangeDataPtr);
-		resSub->m_DataObject = resSubLock.release();
+		auto resultSubChannel = locked_tile_write_channel<PartType>(resSub);
+		resultSubChannel.Write(resSubData.begin(), resSubData.end() );
+		resultSubChannel.Commit();
+		resSubData.reset();
+
 		ResultSub2Type* partLinkUnit = debug_cast<ResultSub2Type*>(resSub2);
 		partLinkUnit->SetRange(Range<PartLinkType>(0, partLinks.size()));
 
-		DataWriteLock partFromLock(resPartFrom, dms_rw_mode::write_only_all);
-		DataWriteLock partToLock(resPartTo, dms_rw_mode::write_only_all);
-		auto partFrom = mutable_array_cast<PartType>(partFromLock);
-		auto partTo = mutable_array_cast<PartType>(partToLock);
-		auto partFromData = partFrom->GetDataWrite(no_tile, dms_rw_mode::write_only_all);
-		auto partToData = partTo->GetDataWrite(no_tile, dms_rw_mode::write_only_all);
-		PartLinkType partLinkIndex = 0;
+		auto partFromChannel = locked_tile_write_channel<PartType>(resPartFrom);
+		auto partToChannel = locked_tile_write_channel<PartType>(resPartTo);
 		for (const auto& partLink : partLinks)
 		{
-			partFromData[partLinkIndex] = partLink.first;
-			partToData[partLinkIndex] = partLink.second;
-			++partLinkIndex;
+			partFromChannel.Write(partLink.first);
+			partToChannel.Write(partLink.second);
 		}
-
-		partFromLock.Commit();
-		partToLock.Commit();
+		partFromChannel.Commit();
+		partToChannel.Commit();
 
 		return true;
 	}
