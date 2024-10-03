ce33b5318240fb1faf91ba95e5df327d8d7174eb
Jip Claassens <jclaassens@objectvision.nl>
2024-10-03 18:58:04 +0200

    Fixed #769

--- shv/dll/src/LayerSet.cpp
+++ shv/dll/src/LayerSet.cpp
@@ -1,36 +1,12 @@
-//<HEADER> 
-/*
-Data & Model Server (DMS) is a server written in C++ for DSS applications. 
-Version: see srv/dms/rtc/dll/src/RtcVersion.h for version info.
-
-Copyright (C) 1998-2004  YUSE GSO Object Vision BV. 
-
-Documentation on using the Data & Model Server software can be found at:
-http://www.ObjectVision.nl/DMS/
-
-See additional guidelines and notes in srv/dms/Readme-srv.txt 
-
-This library is free software; you can use, redistribute, and/or
-modify it under the terms of the GNU General Public License version 2 
-(the License) as published by the Free Software Foundation,
-provided that this entire header notice and readme-srv.txt is preserved.
-
-See LICENSE.TXT for terms of distribution or look at our web site:
-http://www.objectvision.nl/DMS/License.txt
-or alternatively at: http://www.gnu.org/copyleft/gpl.html
-
-This library is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-General Public License for more details. However, specific warranties might be
-granted by an additional written contract for support, assistance and/or development
-*/
-//</HEADER>
+// Copyright (C) 1998-2024 Object Vision b.v. 
+// License: GNU GPL 3
+/////////////////////////////////////////////////////////////////////////////
 
 #include "ShvDllPch.h"
 
 #include "dbg/DebugCast.h"
 #include "dbg/DebugContext.h"
+#include "mci/ValueClassID.h"
 #include "mci/ValueWrap.h"
 
 #include "AbstrUnit.h"
@@ -205,11 +181,17 @@ void AddFoundAspects(GraphicLayer* layer, const AbstrDataItem* viewItem, const L
 	for (AspectNr aNr = AspectNr(1); aNr != AN_AspectCount; ++reinterpret_cast<UInt32&>(aNr) )
 	{
 		if ((AspectNrSet(1<<aNr) & aspectSet)==0) continue;
-
+		if (aNr == AN_LabelText && viewItem->GetAbstrValuesUnit()->GetValueType()->GetValueClassID() == ValueClassID::VT_SharedStr)
+		{
+			auto theme = Theme::Create(aNr, viewItem, nullptr, nullptr);
+			layer->SetThemeAndActivate(theme.get(), viewItem);
+			continue;
+		}
 		LayerInfo aspectInfo = GetAspectInfo(aNr, viewItem, featureInfo, true);
 		if (aspectInfo.m_State == LayerInfo::ConfigError)
 			viewItem->throwItemError(aspectInfo.m_Descr.c_str());
-		if (!aspectInfo.IsComplete()) continue;
+		if (!aspectInfo.IsComplete())
+			continue;
 
 		auto theme = Theme::Create(aNr,
 			aspectInfo.m_diThemeOrGeoRel,
@@ -230,16 +212,16 @@ void AddIndicatedAspects(GraphicLayer* layer, const AbstrDataItem* viewItem, con
 	bool isTextSubLayer =
 		(	labelTextTheme
 		&&	labelTextTheme->GetThemeAttr() == viewItem
-		&&	labelTextTheme->GetClassification() == 0
-		&&	labelTextTheme->GetPaletteAttr() == 0
+		&&	labelTextTheme->GetClassification() == nullptr
+		&&	labelTextTheme->GetPaletteAttr() == nullptr
 		&&  viewItem->GetAbstrValuesUnit()->GetValueType() == ValueWrap<SharedStr>::GetStaticClass() 
 		);
 #if defined(MG_DEBUG)
 	if (isTextSubLayer)
 	{
-		dms_assert(aspectSet & ASE_LabelText);
+		assert(aspectSet & ASE_LabelText);
 		LayerInfo aspectInfo = GetAspectInfo(AN_LabelText, viewItem, featureInfo, true);
-		dms_assert(aspectInfo.IsDirectAspect() && aspectInfo.m_diThemeOrGeoRel == viewItem);
+		assert(aspectInfo.IsDirectAspect() && aspectInfo.m_diThemeOrGeoRel == viewItem);
 	}
 #endif
 
