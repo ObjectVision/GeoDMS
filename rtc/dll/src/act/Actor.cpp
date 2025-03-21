e981e947264c8deb7a207c8a4324f5a7e2e5d2b1
Maarten Hilferink <mhilferink@objectvision.nl>
2025-03-21 18:07:14 +0100

    yet another fence fix

--- rtc/dll/src/act/Actor.cpp
+++ rtc/dll/src/act/Actor.cpp
@@ -546,7 +546,6 @@ void Actor::UpdateSupplMetaInfo() const
 			supplier->UpdateMetaInfo();
 			if (supplier->WasFailed())
 				this->Fail(supplier);
-			MakeMax<fence_number>(this->m_FenceNumber, supplier->m_FenceNumber);
 		}
 	);
 }
