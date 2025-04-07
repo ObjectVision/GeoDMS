74b0a9c9b9a377189d181a35b7569f4448497e5d
Maarten Hilferink <mhilferink@objectvision.nl>
2025-04-08 01:43:28 +0200

    reproduction case for https://github.com/boostorg/geometry/issues/1383

--- tic/tst/src/Main.cpp
+++ tic/tst/src/Main.cpp
@@ -1,90 +1,51 @@
-#include "RtcInterface.h"
-#include "ClcInterface.h"
-//#include "GeoInterface.h"
-//#include "ShvInterface.h"
+#include "boost/geometry.hpp"
+#include "boost/geometry/algorithms/union.hpp"
 
-#include "MlModel.h"
-#include "SystemTest.h"
+namespace bg = boost::geometry;
 
-#include <iostream>
-#include <functional>
-#include <algorithm>
-#include <vector>
+constexpr bool ClockWise = true;
+constexpr bool Closed    = true; // each ring is closed, i.e. last point == first point
 
-// ============== Main
-
-#include "dbg/debug.h"
-#include "dbg/DebugLog.h"
-#include "dbg/DmsCatch.h"
-#include "set/IndexedStrings.h"
-#include "utl/Environment.h"
-#include "utl/splitPath.h"
-
-void reportErr(CharPtr errMsg)
-{
-	std::cerr << std::endl << errMsg;
-}
-
-enum Test : short { a = true };
-enum : int { b = 1 };
+using Point        = bg::model::point<double, 2, bg::cs::cartesian>;
+using Ring         = bg::model::ring   <Point, ClockWise, Closed>;
+using Polygon      = bg::model::polygon<Point, ClockWise, Closed>;
+using MultiPolygon = bg::model::multi_polygon<Polygon>;
 
 int main(int argc, char** argv)
 {
-	std::cout << "kerstpzzl" << std::endl;
-	for (int k = 1; k != 10; k++)
-	for (int e = 1; e != 10; e++) // if (e!=k)
-	for (int r = 1; r != 10; r++) // if (e != r) if (r != k)
-	for (int s = 1; s != 10; s++) // if (e != s) if (s != k) if (s != r)
-	for (int t = 1; t != 10; t++) // if (e != t) if (t != k) if (t != r) if (t!= s)
-	for (int i = 1; i != 10; i++) // if (e != i) if (i != k) if (i != r) if (s != i) if (i!=t)
-	for (int l = 1; l != 10; l++) // if (e != l) if (l != k) if (l != r) if (s != l) if (l != t) if (l!=i)
-	for (int m = 1; m != 10; m++) // if (m != i) if (e != m) if (m != k) if (m != r) if (s != m) if (m != t) if (l != m)
-	for (int n = 1; n != 10; n++) // if (e != n) if (n != k) if (n != r) if (s != n) if (n != t) if (n != i) if (l != n) if (n != m)
-		if (
-			(((((k * 10 + e) * 10 + r) * 10 + s) * 10) + t)
-			+ ((((((l * 10 + e) * 10 + t) * 10 + t) * 10 + e) * 10 + r) * 10 + s)
-			==
-			((((((r * 10 + e) * 10 + k) * 10 + e) * 10 + n) * 10 + e) * 10 + n)
-			+ (((m * 10) + e) * 10 + t) * (((t * 10 + i) * 10 + e) * 10 + n)
-		)
+	Ring s1{
+		{ 173904.25160630842, 604340     }, // A
+		{ 173803.203125     , 604351.875 }, // B
+		{ 173802.59375      , 604351.9375}, // C
+		{ 173802.578125     , 604351.9375}, // D
+		{ 173797.5588235294 , 604360     }, // E
+		{ 174000            , 604360     }, // F
+		{ 174000            , 604340     }, // G
+		{ 173904.25160630842, 604340     }  // A
+	};
+	std::cout << "area of s1 = " << bg::area(s1) << std::endl;
+
+	Ring s2{
+		{ 173797.5588235294 , 604360     }, // E
+		{ 173802.578125     , 604351.9375}, // D
+		{ 173802.59375      , 604351.9375}, // C
+		{ 173794.765625     , 604352.875 }, // H
+		{ 173735.58701923076, 604360     }, // I
+		{ 173797.5588235294 , 604360     }  // E
+	};
+	std::cout << "area of s2 = " << bg::area(s2) << std::endl;
+
+	MultiPolygon result;
+	bg::union_(s1, s2, result);
+
+	if (result.empty())
+		std::cout << "result of union(s1, s2) is empty" << std::endl;
+	else for (auto r: result)
+	{
+		std::cout << "outer" << std::endl;
+		for (auto r : r.outer())
 		{
-			std::cout << "MINSTREEL: " << m << i << n << s << t << r << e << e << l << std::endl;
-			std::cout << "REKENLES: " << r << e << k << e << n << l << e << s << std::endl;
+			std::cout << bg::get<0>(r) << " " << bg::get<1>(r) << std::endl;
 		}
-	return 0;
-
-	CDebugLog tracelog(SharedStr("c:/LocalData/Trace.log"));
-
-/*
-	IndexedStrings ivec;
-	GetDir("C:\\LocalData\\por\\CalcCache\\*", ivec);
-	return 0;
-
-	ThreeKPlusOne();
-	return 0;
-*/
-
-//	SharedStr applFullName = SharedStr( argv[0] );
-//	std::replace(applFullName.begin(), applFullName.send(), '\\', DELIMITER_CHAR);
-//	DMS_Appl_SetExeDir( splitFullPath( ConvertDosFileName(applFullName) ).c_str() );
-	DMS_Appl_SetExeDir( splitFullPath( ConvertDosFileName(SharedStr( argv[0] )) .c_str()).c_str() );
-
-	DBG_START("Main", "", true);
-	DMS_SetGlobalCppExceptionTranslator(reportErr);
-
-	DMS_CALL_BEGIN
-
-		bool result = true;
-
-		result &= DmsSystemTest();
-		result &= testML();
-
-		if (!result)
-			return 1;
-
-		return 0;
-
-	DMS_CALL_END
-	return 1;
-
+	}
 }
