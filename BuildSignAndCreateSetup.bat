14045591dced720e4670eb565f7dfecbed3b976f
MaartenHilferink <mhilferink@objectvision.nl>
2025-07-30 16:42:43 +0200

    Prep for 18.0.0

--- BuildSignAndCreateSetup.bat
+++ BuildSignAndCreateSetup.bat
@@ -1,9 +1,9 @@
 echo on
 cls
 
-set DMS_VERSION_MAJOR=17
-set DMS_VERSION_MINOR=9
-set DMS_VERSION_PATCH=7
+set DMS_VERSION_MAJOR=18
+set DMS_VERSION_MINOR=0
+set DMS_VERSION_PATCH=0
 
 set geodms_rootdir=%cd%
 
