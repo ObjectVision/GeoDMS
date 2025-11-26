a6b288af6f43585425a827584f168f8d50620877
MaartenHilferink <mhilferink@objectvision.nl>
2025-11-26 17:42:59 +0100

    Splash screen 75% of current screen width and height; removed HackedWorld; white status text

--- qtgui/exe/src/DmsSplashScreen.cpp
+++ qtgui/exe/src/DmsSplashScreen.cpp
@@ -11,13 +11,6 @@ void DmsSplashScreen::drawContents(QPainter* painter) {
     painter->drawText(this->m_rect, this->m_alignment, this->m_message);
 }
 
-void DmsSplashScreen::showStatusMessage(const QString& message, const QColor& color) 
-{
-    this->m_message = message;
-    this->m_color = color;
-    this->showMessage(this->m_message, this->m_alignment, this->m_color);
-}
-
 void DmsSplashScreen::setMessageRect(QRect rect, int alignment) 
 {
     this->m_rect = rect;
@@ -30,22 +23,44 @@ auto showSplashScreen() -> std::unique_ptr<DmsSplashScreen>
     QString family = QFontDatabase::applicationFontFamilies(id).at(0);
     QFont dms_text_font(family, 25);
 
-//    QPixmap pixmap(":/res/images/HackedWorld.jpg");
     QPixmap pixmap(":/res/images/WorldHeatMap.jpg");
-    std::unique_ptr<DmsSplashScreen> splash = std::make_unique<DmsSplashScreen>(pixmap);
-    splash->setMessageRect(QRect(splash->rect().topLeft(), QSize(1024, 200)), Qt::AlignCenter);
-    dms_text_font.setBold(true);
-    splash->setFont(dms_text_font);
 
     auto app = dynamic_cast<QApplication*>(QCoreApplication::instance());
     assert(app);
     auto screen_at_mouse_pos = app->screenAt(QCursor::pos());
-    const QPoint currentDesktopsCenter = screen_at_mouse_pos->geometry().center();
-    assert(splash->rect().top() == 0);
-    assert(splash->rect().left() == 0);
-    auto projectedTopLeft = currentDesktopsCenter - splash->rect().center();
-    if (projectedTopLeft.y() < screen_at_mouse_pos->geometry().top())
-        projectedTopLeft.setY(screen_at_mouse_pos->geometry().top());
+    QScreen* screen = screen_at_mouse_pos ? screen_at_mouse_pos : app->primaryScreen();
+    const QRect screenGeom = screen->availableGeometry(); // use availableGeometry to avoid taskbar
+    const double scaleFactor = 0.75;
+
+    // target size = 75% of current screen, keep pixmap aspect ratio
+    const QSize targetSize(qRound(screenGeom.width() * scaleFactor),
+        qRound(screenGeom.height() * scaleFactor));
+    QPixmap scaledPixmap = pixmap.scaled(targetSize, Qt::KeepAspectRatio, Qt::SmoothTransformation);
+
+    std::unique_ptr<DmsSplashScreen> splash = std::make_unique<DmsSplashScreen>(scaledPixmap);
+
+    // place message rect relative to splash size (bottom band)
+    const QRect srect = splash->rect();
+    const QSize msgSize(srect.width(), qRound(srect.height() * 0.15)); // 15% of splash height
+    splash->setMessageRect(QRect(QPoint(srect.left(), srect.bottom() - msgSize.height()), msgSize), Qt::AlignCenter);
+
+    dms_text_font.setBold(true);
+    splash->setFont(dms_text_font);
+    splash->m_color = QColor(255, 255, 255);
+
+    // position splash centered on the screen containing the mouse, but make sure it stays on-screen
+    const QPoint screenCenter = screenGeom.center();
+    auto projectedTopLeft = screenCenter - splash->rect().center();
+    if (projectedTopLeft.y() < screenGeom.top())
+        projectedTopLeft.setY(screenGeom.top());
+    if (projectedTopLeft.x() < screenGeom.left())
+        projectedTopLeft.setX(screenGeom.left());
+    // ensure splash fully visible horizontally and vertically
+    if (projectedTopLeft.x() + splash->rect().width() > screenGeom.right() + 1)
+        projectedTopLeft.setX(screenGeom.right() + 1 - splash->rect().width());
+    if (projectedTopLeft.y() + splash->rect().height() > screenGeom.bottom() + 1)
+        projectedTopLeft.setY(screenGeom.bottom() + 1 - splash->rect().height());
+
     splash->move(projectedTopLeft);
     splash->show();
     return splash;
